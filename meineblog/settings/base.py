fbc9e822432efb7d3c1c6e0e3732b54bed2d90ec
Obeth <torresobeth@gmail.com>
2023-04-13 20:09:57 +0200

    hosts again

--- meineblog/settings/base.py
+++ meineblog/settings/base.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://15.188.64.107:8000",
+    "http://15.188.64.107",
 ]
 
 ALLOWED_HOSTS = ['15.188.64.107']
